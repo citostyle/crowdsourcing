52ed487b3813d012e892ea3369f0fe6bd987ac3a
Martin Planer <e0925719@mail.student.tuwien.ac.at>
2012-10-22 15:41:57 +0200

    Spring, Spring MVC + Hibernate Grundgeruest

---
+++ Crowdsourcing/src/main/java/tuwien/aic/crowdsourcing/persistence/dao/TestEntityDAO.java
@@ -0,0 +1,9 @@
+package tuwien.aic.crowdsourcing.persistence.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import tuwien.aic.crowdsourcing.persistence.entities.TestEntity;
+
+public interface TestEntityDAO extends JpaRepository<TestEntity, Long> {
+
+}
