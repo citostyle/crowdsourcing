c4731851b06041b122361bbc6cfd61f8b327a5d5
Martin Planer <e0925719@mail.student.tuwien.ac.at>
2012-10-23 19:38:55 +0200

    Added test implementations for JSON REST API and Scheduled tasks

---
+++ Crowdsourcing/src/main/java/tuwien/aic/crowdsourcing/JSONController.java
@@ -0,0 +1,42 @@
+package tuwien.aic.crowdsourcing;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import tuwien.aic.crowdsourcing.persistence.entities.TestEntity;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class JSONController {
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(JSONController.class);
+
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/json/{id}", method = RequestMethod.GET)
+	@ResponseBody
+	public TestEntity get(@PathVariable String id) {
+		
+		logger.info("Generating JSON response for id " + id);
+		
+		TestEntity testEntity = new TestEntity();
+
+		testEntity.setName(id);
+		try {
+			testEntity.setId(Long.valueOf(id));
+		} catch (NumberFormatException e) {
+		}
+
+		return testEntity;
+	}
+
+}
---
+++ Crowdsourcing/src/main/java/tuwien/aic/crowdsourcing/ScheduleTest.java
@@ -0,0 +1,17 @@
+package tuwien.aic.crowdsourcing;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduleTest {
+	
+	private static final Logger logger = LoggerFactory.getLogger(ScheduleTest.class);
+
+	@Scheduled(fixedRate = 5000)
+	public void doWork() {
+		logger.info("Arbeit, Arbeit...");
+	}
+}
