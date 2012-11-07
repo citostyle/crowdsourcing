1ee461d9436af1c467ed8627f9333e789866d397
Colossus <e0902998@student.tuwien.ac.at>
2012-11-07 14:30:19 +0100

    added article parser

---
+++ Crowdsourcing/src/main/java/tuwien/aic/crowdsourcing/rss/ArticleParser.java
@@ -0,0 +1,79 @@
+package tuwien.aic.crowdsourcing.rss;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import tuwien.aic.crowdsourcing.persistence.CompanyManager;
+import tuwien.aic.crowdsourcing.persistence.ProductManager;
+import tuwien.aic.crowdsourcing.persistence.entities.Company;
+import tuwien.aic.crowdsourcing.persistence.entities.Product;
+
+@Component
+public class ArticleParser {
+
+    @Autowired
+    private ProductManager productManager;
+
+    @Autowired
+    private CompanyManager companyManager;
+
+    private boolean foundProduct(String text, Product product) {
+        String lower = text.toLowerCase();
+        if (lower.contains(product.getName().toLowerCase())) {
+            return true;
+        }
+        for (String syn : product.getSynonyms()) {
+            if (lower.contains(syn.toLowerCase())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public List<Product> getProductsInArticle(String url) throws IOException {
+        List<Product> allProducts = productManager.findAll();
+
+        Document document = Jsoup.connect(url).get();
+        List<Product> ret = new ArrayList<Product>();
+        String text = document.body().text();
+        for (Product p : allProducts) {
+            if (foundProduct(text, p)) {
+                ret.add(p);
+            }
+        }
+        return ret;
+    }
+
+    private boolean foundCompany(String text, Company product) {
+        String lower = text.toLowerCase();
+        if (lower.contains(product.getName().toLowerCase())) {
+            return true;
+        }
+        for (String syn : product.getSynonyms()) {
+            if (lower.contains(syn.toLowerCase())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public List<Company> getCompaniesInArticle(String url) throws IOException {
+        List<Company> allProducts = companyManager.findAll();
+
+        Document document = Jsoup.connect(url).get();
+        List<Company> ret = new ArrayList<Company>();
+        String text = document.body().text();
+        for (Company p : allProducts) {
+            if (foundCompany(text, p)) {
+                ret.add(p);
+            }
+        }
+        return ret;
+    }
+}
---
+++ Crowdsourcing/src/test/java/tuwien/aic/crowdsourcing/rss/ArticleParserTest.java
@@ -0,0 +1,63 @@
+package tuwien.aic.crowdsourcing.rss;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import tuwien.aic.crowdsourcing.persistence.CompanyManager;
+import tuwien.aic.crowdsourcing.persistence.ProductManager;
+import tuwien.aic.crowdsourcing.persistence.entities.Company;
+import tuwien.aic.crowdsourcing.persistence.entities.Product;
+
+public class ArticleParserTest {
+
+    @Mock
+    private ProductManager productManager;
+
+    @Mock
+    private CompanyManager companyManager;
+
+    @Before
+    public void initMocks() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @InjectMocks
+    private ArticleParser a = new ArticleParser();
+
+    @Test
+    public void testGetProductsInArticle() throws IOException {
+        Product p1 = new Product(null, "Exchange");
+        Product p2 = new Product(null, "Exchangeasdfasdfasdf");
+        Mockito.when(productManager.findAll())
+                .thenReturn(Arrays.asList(p1, p2));
+        List<Product> ret = a.getProductsInArticle("http://finance.yah"
+                + "oo.com/news/why-couldnt-wall"
+                + "-street-weather-054400413.html");
+        assertTrue(ret.contains(p1));
+        assertFalse(ret.contains(p2));
+    }
+
+    @Test
+    public void testGetCompaniesInArticle() throws IOException {
+        Company p1 = new Company("Exchange");
+        Company p2 = new Company("Exchangeasdfasdfasdf");
+        Mockito.when(companyManager.findAll())
+                .thenReturn(Arrays.asList(p1, p2));
+        List<Company> ret = a.getCompaniesInArticle("http://finance.yah"
+                + "oo.com/news/why-couldnt-wall"
+                + "-street-weather-054400413.html");
+        assertTrue(ret.contains(p1));
+        assertFalse(ret.contains(p2));
+    }
+}
