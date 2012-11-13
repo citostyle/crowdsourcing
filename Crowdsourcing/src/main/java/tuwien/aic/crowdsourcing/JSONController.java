47acc83b16be02713e635a2481cee7bf3577fb23
Martin Planer <e0925719@mail.student.tuwien.ac.at>
2012-11-13 21:04:47 +0100

    Added search controller + ApiService

--- Crowdsourcing/src/main/java/tuwien/aic/crowdsourcing/JSONController.java
+++ Crowdsourcing/src/main/java/tuwien/aic/crowdsourcing/JSONController.java
@@ -1,15 +1,23 @@
 package tuwien.aic.crowdsourcing;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import tuwien.aic.crowdsourcing.persistence.CompanyManager;
 import tuwien.aic.crowdsourcing.persistence.entities.Company;
 import tuwien.aic.crowdsourcing.persistence.entities.TestEntity;
+import tuwien.aic.crowdsourcing.service.ApiService;
 import tuwien.aic.crowdsourcing.web.JsonCompany;
 import tuwien.aic.crowdsourcing.web.JsonList;
 
@@ -21,6 +29,10 @@ public class JSONController {
 
     private static final Logger logger = LoggerFactory
             .getLogger(JSONController.class);
+    
+    @Autowired
+    private ApiService apiService;
+    
 
     /**
      * Simply selects the home view to render by returning its name.
@@ -81,4 +93,26 @@ public class JSONController {
         
         return new JsonList(true, true);
     }
+    
+    /**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/search/{expr}", method = RequestMethod.GET)
+	@ResponseBody
+	public List<JsonCompany> searchCompanies(@PathVariable String expr) {
+		
+		logger.info("Searching for: " + expr);
+		
+		ArrayList<JsonCompany> result = new ArrayList<JsonCompany>();
+
+		List<Company> companies = apiService.searchCompany(expr);
+		
+		for(Company c : companies)
+		{
+			JsonCompany company = new JsonCompany(c);
+			result.add(company);
+		}
+		
+		return result;
+	}
 }
