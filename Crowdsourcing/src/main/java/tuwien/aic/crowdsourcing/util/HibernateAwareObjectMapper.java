247c23a5acdfee7fc0777eeb72a6cba93cd2c526
Martin Planer <e0925719@mail.student.tuwien.ac.at>
2012-11-15 14:35:31 +0100

    JSON REST API Prototyp (hat noch Probleme mit Hibernate :/ )

---
+++ Crowdsourcing/src/main/java/tuwien/aic/crowdsourcing/util/HibernateAwareObjectMapper.java
@@ -0,0 +1,15 @@
+package tuwien.aic.crowdsourcing.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
+import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module.Feature;
+
+public class HibernateAwareObjectMapper extends ObjectMapper {
+
+	private static final long serialVersionUID = 1L;
+
+	public HibernateAwareObjectMapper() {
+		Hibernate4Module hm = new Hibernate4Module();
+		registerModule(hm);
+	}
+}
